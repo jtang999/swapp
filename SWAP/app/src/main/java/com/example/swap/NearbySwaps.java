23f74fb38450689e41cbf022285b45461c42cb85
joshua-baum <joshuabaum@berkeley.edu>
2020-11-15 19:04:47 -0800

    adding header, search bar, toggle buttons(in progress)

--- SWAP/app/src/main/java/com/example/swap/NearbySwaps.java
+++ SWAP/app/src/main/java/com/example/swap/NearbySwaps.java
@@ -1,5 +1,6 @@
 package com.example.swap;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,23 +10,60 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
 
 public class NearbySwaps extends AppCompatActivity {
-
+    public static int toggle = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nearby_swaps);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        initializeToggles();
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+    }
+
+    public void initializeToggles(){
+        Button freeServices = findViewById(R.id.freeServicesToggle); //Option 0
+        freeServices.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                setToggle(0);
             }
         });
+        Button swap = findViewById(R.id.swapToggle); //Option 1
+        freeServices.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setToggle(1);
+            }
+        });
+        Button needsFree = findViewById(R.id.needsFreeToggle); //Option 2
+        freeServices.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setToggle(2);
+            }
+        });
+    }
+
+    public void setToggle(int newToggle){
+        NearbySwaps.toggle = newToggle;
+        Button freeServices = findViewById(R.id.freeServicesToggle); //Option 0
+        Button swap = findViewById(R.id.swapToggle); //Option 1
+        Button needsFree = findViewById(R.id.needsFreeToggle); //Option 2
+        if(NearbySwaps.toggle == 0){
+            freeServices.setBackground(Drawable.createFromPath("@drawable/createbutton"));
+            swap.setBackground(Drawable.createFromPath("@drawable/modetoggle"));
+            needsFree.setBackground(Drawable.createFromPath("@drawable/modetoggle"));
+        }else if (NearbySwaps.toggle == 1){
+            freeServices.setBackground(Drawable.createFromPath("@drawable/modetoggle"));
+            swap.setBackground(Drawable.createFromPath("@drawable/createbutton"));
+            needsFree.setBackground(Drawable.createFromPath("@drawable/modetoggle"));
+        }else{
+            freeServices.setBackground(Drawable.createFromPath("@drawable/modetoggle"));
+            swap.setBackground(Drawable.createFromPath("@drawable/modetoggle"));
+            needsFree.setBackground(Drawable.createFromPath("@drawable/createbutton"));
+        }
     }
 }
