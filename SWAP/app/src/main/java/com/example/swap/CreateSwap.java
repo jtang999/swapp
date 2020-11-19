6c80f9508bc1c0b9708e8b7821f7e9508b41df4a
Andrew Haitz <andrew.haitz@berkeley.edu>
2020-11-19 00:27:09 -0800

    Linked CreateSwap Button, Created Create Swap Class, Fixed SwitchCompat Bug, got rid of actionbar.
    Setup for working on Firebase storage for swap creation.

---
+++ SWAP/app/src/main/java/com/example/swap/CreateSwap.java
@@ -0,0 +1,16 @@
+package com.example.swap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.widget.SwitchCompat;
+
+public class CreateSwap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_swap);
+    }
+}
